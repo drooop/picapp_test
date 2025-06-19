677cb37d4819bc26e9aa80a2f8df16cbb420c529
Kevin_Yang <39990043+drooop@users.noreply.github.com>
2025-06-19 10:43:44 +0800

    Add Deno tasks

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-container style="height:100vh">
+    <el-main>
+      <el-button @click="runPython">Run Python</el-button>
+      <div>{{ output }}</div>
+    </el-main>
+  </el-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { invoke } from '@tauri-apps/api'
+const output = ref('')
+const runPython = async () => {
+  output.value = await invoke('run_python')
+}
+</script>
